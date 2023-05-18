47a157e752e58507af53d19d7ee634c97f4d3386
Yelena Strizhakova <e.strizhakova@gmail.com>
2023-05-18 16:20:35 +0400

    security modules added

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const router = require('./routes.js');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 
+
 const { PORT = 3000 } = process.env;
 const app = express();
 
+app.use(helmet());
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100
+});
+app.use(limiter);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -21,6 +30,18 @@ app.use('/', router);
 app.use(express.static(__dirname + '/public'));
 
 app.use(errors());
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500
+        ? 'на сервере произошла ошибка'
+        : message
+    });
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
